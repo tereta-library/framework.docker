0351e449fabea499c9ad319f8e402e54835480d2
Tereta Alexander <atereta@gomage.com>
2024-07-21 16:13:35 +0300

    Version 0.0.9

---
+++ src/Module.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace Framework\Docker;
+
+use Framework\Application\Interface\Module as ModuleInterface;
+
+/**
+ * ···························WWW.TERETA.DEV······························
+ * ·······································································
+ * : _____                        _                     _                :
+ * :|_   _|   ___   _ __    ___  | |_    __ _        __| |   ___  __   __:
+ * :  | |    / _ \ | '__|  / _ \ | __|  / _` |      / _` |  / _ \ \ \ / /:
+ * :  | |   |  __/ | |    |  __/ | |_  | (_| |  _  | (_| | |  __/  \ V / :
+ * :  |_|    \___| |_|     \___|  \__|  \__,_| (_)  \__,_|  \___|   \_/  :
+ * ·······································································
+ * ·······································································
+ *
+ * @class Framework\Docker\Module
+ * @package Framework\Docker
+ * @link https://tereta.dev
+ * @author Tereta Alexander <tereta.alexander@gmail.com>
+ */
+class Module implements ModuleInterface
+{
+}
